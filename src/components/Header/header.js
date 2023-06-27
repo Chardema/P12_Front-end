58d07fc23f6827447d7025af49ca7088c6baccce
HARDEMAN CHRISTOPHER <41175936+Chardema@users.noreply.github.com>
2023-06-27 16:15:47 +0200

    début du passage en react, travaille sur le component header

---
+++ src/components/Header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./header.module.scss"
+
+const Header = () => {
+    return (
+        <head>
+            <title>HRnet - Current Employees</title>
+        </head>
+    );
+};
+
+export default Header;
